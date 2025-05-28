9ed514ffa14ce6ff8eb2a7195aa748704a8ffe26
Jakub Wiszowaty <jakubwiszowaty@hotmail.com>
2025-05-28 23:56:36 +0200

    mongoDB and mongoose tasks

--- myApp.js
+++ myApp.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
-
-
+const mongoose = require("mongoose")
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 let Person;
-
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
